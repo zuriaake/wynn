ec5d93483dec502bb7e61b43afc6fd28f9fa351d
Wynn Netherland <wynn.netherland@gmail.com>
2010-06-08 15:41:27 -0500

    updated content

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,20 @@
 // Put your application scripts here
+
+jQuery(document).ready(function($) {
+  
+  $('.vimeo').each(function(index, element) {
+    var videoId = $(element).attr('data-video-id');
+    if (videoId) {
+      if (Modernizr.video) {
+        var src = 'http://www.vimeo.com/play_redirect?clip_id='+videoId+'&quality=mobile';
+        var player = $('<video>')
+          .attr('src', src)
+          .attr('controls', 'controls')
+          .attr('width', '550')
+          .attr('height', '309');
+        $(this).append(player);
+      }
+    }
+  });
+  
+});
