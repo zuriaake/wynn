ae5549212582052a16357b39e24e23803451bfad
Wynn Netherland <wynn.netherland@gmail.com>
2011-09-17 20:16:52 -0500

    404

--- themes/wynn/app.rb
+++ themes/wynn/app.rb
@@ -5,6 +5,17 @@ module Nesta
     def speaker_deck_thumb_url(id)
       "//speakerd.s3.amazonaws.com/presentations/#{id}/thumb_slide_0.jpg"
     end
+
+
+    not_found do
+      set_common_variables
+      haml(:not_found, :layout => :centered)
+    end
+
+    error do
+      set_common_variables
+      haml(:error, :layout => :centered)
+    end unless Nesta::App.development?
   end
 
   class FileModel
