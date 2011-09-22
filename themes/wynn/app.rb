b3d04c06c2814aca974d110f991d531a23ad65d6
Wynn Netherland <wynn.netherland@gmail.com>
2011-09-22 10:02:21 -0500

    fixes feed, having to drop in sinatra route

--- themes/wynn/app.rb
+++ themes/wynn/app.rb
@@ -29,6 +29,13 @@ module Nesta
       haml(:error, :layout => :centered)
     end unless Nesta::App.development?
 
+    get '/articles.xml' do
+      content_type :xml, :charset => 'utf-8'
+      set_from_config(:title, :subtitle, :author)
+      @articles = Page.find_articles.select { |a| a.date }[0..9]
+      cache haml(:atom, :format => :xhtml, :layout => false)
+    end
+
     get '*' do
       set_common_variables
       @heading = @title
