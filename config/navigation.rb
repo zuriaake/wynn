04b2836da264e382144176d4a10a746562205478
Wynn Netherland <wynn@github.com>
2013-01-04 16:09:22 -0600

    About page

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,4 +1,4 @@
-SimpleNavigation::Configuration.run do |navigation|  
+SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'site-nav'
     primary.item :journal, 'Journal', '/', :highlights_on => /^journal|linked|til/
