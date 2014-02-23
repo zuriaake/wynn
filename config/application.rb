610ce93742907e386d7307978462e470ac34ad06
Wynn Netherland <wynn@github.com>
2014-02-23 15:09:08 -0600

    Correct canonical conditional

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ module Blog
 
       unless Rails.env.development?
         r301 %r{.*}, 'http://wynnnetherland.com$&', :if => Proc.new {|rack_env|
-          rack_env['SERVER_NAME'] == 'wynnnetherland.com' &&
+          rack_env['SERVER_NAME'] != 'wynnnetherland.com' &&
             rack_env['SERVER_NAME'] !~ /stg/
         }
       end
